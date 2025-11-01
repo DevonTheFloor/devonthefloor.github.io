a9cfd484c0380146781e25ee365529a5499e18ea
DevonTheFloor <devonthefloor@free.fr>
2025-11-01 12:07:46 +0100

    Fix add footer

---
+++ _nuxt/static/1761995192/blog/tutoriel-vite-js-bases/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1761995192",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Ftutoriel-vite-js-bases",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b8bd10bf"}}}}(null));
