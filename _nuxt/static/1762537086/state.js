f8bf718d26d93488761693bdb20bf2335960c3c1
DevonTheFloor <devonthefloor@free.fr>
2025-11-07 18:39:27 +0100

    Fix year footer

---
+++ _nuxt/static/1762537086/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1762537086",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"25eca47e"}}}}(null,"\u002F"));
